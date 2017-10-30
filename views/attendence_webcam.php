d7ab79e33526f57fa8922ce5b500631c216a8ba7
Arun-sophos <2015uec1283@mnit.ac.in>
2017-10-30 10:56:50 +0000

    Third Commit

--- views/attendence_webcam.php
+++ views/attendence_webcam.php
@@ -1,11 +1,17 @@
-<div style="width:100%; margin:0 auto;">
-        <div width="45%" style="float:left;border:2px solid red;margin-right=2px;padding:4px 4px 4px 4px">
-            <video id='video' width='400' height='280'></video>     </br><center>
+<center>
+<div style="width:70%; margin:0 auto;">
+        <!--width="45%" style="float:left;border:2px solid red;margin-right=2px;padding:4px 4px 4px 4px"-->
+        <div width="600" style="float:left;border:3px ridge blue;border-radius: 5px;">
+            <video id='video' width='373' height='280'></video></br><center>
             <button id='btn_snap' onclick='take_photo()'>Cheese</button></center>
         </div>
-        <div width = "45%" style = 'float:right;border:2px solid green;padding:4px 4px 4px 4px'>
-            <canvas id='canvas' width='400' height='280' ></canvas>      </br><center>
+         <!--width = "45%" style = 'float:right;border:2px solid green;padding:4px 4px 4px 4px'-->
+        <div width="600" style="float:right;border:3px ridge blue;border-radius: 5px;">
+            <canvas id='canvas' width='400' height='276' style="border:2px inset yellow"></canvas>      </br><center>
             <input type='text' name='filename' id='filename' >&nbsp;&nbsp;
             <button id='btn_save' onclick='save_photo()' >Save</button></center>
         </div>
 </div>
+<br/>
+</br>
+</center>
